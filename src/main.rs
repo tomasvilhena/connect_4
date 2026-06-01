90c4d9a3207a4bdb7ea779f29be51cd66d7c1399
tomasvilhena <164338580+tomasvilhena@users.noreply.github.com>
2026-06-01 15:35:27 +0200

    selector color and renderization. improved board style to mathc connect 4 better

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::io::stdout;
+
 use colored::{self, Colorize};
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::{execute, terminal::{Clear, ClearType}, cursor::MoveTo};
+
+use crate::Player::Player1;
 
 #[derive(PartialEq, Clone, Copy, Debug)]
-enum CellState 
+enum CellState
 {
   Empty,
   Occupied(Player),
@@ -9,92 +16,93 @@ enum CellState
 }
 
 #[derive(PartialEq, Clone, Copy, Debug)]
-enum Player 
+enum Player
 {
   Player1,
   Player2,
 }
 
-fn print_board(board: &Vec<Vec<CellState>>) 
+fn print_board(board: &Vec<Vec<CellState>>, selected: (usize, usize), current_player: Player)
 {
-  let mut is_line: bool = true;
+  let mut is_line: bool;
   let mut is_first: bool = true;
-  
+
   for y in 0..13
   {
     for x in 0..15
     {
-      if board[y][x] == CellState::HasWall 
+      if board[y][x] == CellState::HasWall
       {
-        if y == 0 || y % 2 == 0 
+        if y % 2 == 0 && y != 0
         {
-          if x == 0 || x % 2 == 0 
+          if x == 0 || x % 2 == 0
           {
             is_line = true;
-          } else 
+          } else
           {
             is_line = false;
           }
-          
-          if is_line 
+
+          if is_line
           {
-            if is_first 
+            if is_first
             {
               print!("+");
               is_first = !is_first;
             }
 
-            if x <= 13 
+            if x <= 13
             {
-              print!("---");
-            } 
-          } else 
+              if y == 12
+              {
+                print!("---");
+              } else
+              {
+                print!("   ");
+              }
+            }
+          } else
           {
             print!("+");
           }
           continue;
-        } else if x == 0 || x % 2 == 0 
+        } else if x % 2 == 0 && y != 0
         {
           print!("|");
         }
-      } else if board[y][x] == CellState::Empty 
+      } else if y == selected.1 && x == selected.0 && board[selected.0][selected.1] == CellState::Empty
+      {
+        if current_player == Player::Player1 
+        {
+          print!("{}", "▇▇▇".bright_green());
+        } else if current_player == Player::Player2
+        {
+          print!("{}", "▇▇▇".bright_red());
+        }
+      } else if board[y][x] == CellState::Empty
       {
         print!("   ");
-      } else if board[y][x] == CellState::Occupied(Player::Player1) 
+      } else if board[y][x] == CellState::Occupied(Player::Player1)
       {
         print!("{}", "▇▇▇".bright_yellow());
-      }  else if board[y][x] == CellState::Occupied(Player::Player2) 
+      } else if board[y][x] == CellState::Occupied(Player::Player2)
       {
         print!("{}", "▇▇▇".bright_blue());
       }
     }
 
-    
     is_first = true;
-    println!();
+    print!("\r\n");
   }
 }
 
-fn set_up_board(board: &mut Vec<Vec<CellState>>) 
+fn set_up_board(board: &mut Vec<Vec<CellState>>)
 {
-  for y in 0..13 
+  for y in 0..13
   {
-    for x in 0..15 
+    for x in 0..15
     {
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      //  wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall  wall wall
-      //  wall empty wall empty wall empty wall empty wall empty wall empty wall empty wall
-      // 
-      if y == 0 || y % 2 == 0 || x == 0 || x % 2 == 0 
+      if y == 0 || y % 2 == 0 || x == 0 || x % 2 == 0
       {
         board[y][x] = CellState::HasWall;
         continue
@@ -103,10 +111,57 @@ fn set_up_board(board: &mut Vec<Vec<CellState>>)
   }
 }
 
-fn main() 
+fn check_for_win(board: &Vec<Vec<CellState>>) 
 {
+  //for every cell
+  // you need to find all adjacent cells that could make a 4 in a row
+  // for that to be possible oyud chekc all 4 cells above, 
+  // all for 4 cells bellow
+  // all cells above and below togheter becuas eit could be centered and then have one below and stuf like that
+  // and all fot hat for diagonal 2
+  // while going form odd to odd number
+  // 
+  // 
+}
+
+fn main()
+{
+  enable_raw_mode().unwrap();
   let mut board: Vec<Vec<CellState>> = vec![vec![CellState::Empty; 15]; 13];
+  let mut selected = (1, 11);
+  let mut current_player = Player::Player1;
+  execute!(stdout(), Clear(ClearType::All), MoveTo(0,0)).unwrap();
   set_up_board(&mut board);
-  println!("{:#?}", board);
-  print_board(&board);
+  print_board(&board, selected, current_player);
+
+  'game_loop: loop
+  {
+    if let Event::Key(event) = read().unwrap()
+    {
+      match event.code
+      {
+        KeyCode::Left =>
+        {
+          
+        },
+
+        KeyCode::Right =>
+        {
+
+        },
+
+        KeyCode::Enter =>
+        {
+
+        },
+
+        KeyCode::Esc =>
+        {
+          std::process::exit(0);
+        },
+
+        _ => {},
+      }
+    }
+  }
 }
